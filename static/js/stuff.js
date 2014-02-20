8466bbbee5466e24fbc799d3e76d5c410c12548f
harrisonthai <swimboy360@gmail.com>
2014-02-19 19:16:18 -0800

    completed hello world problem!

---
+++ static/js/stuff.js
@@ -0,0 +1,24 @@
+alert('hello there welcome. this is stuff. i hope you enjoy.'); // edit me!
+
+// Problem 1 (Say Hello!) ---------------------------------------------------
+$('#say_hello').click(function() {
+alert('hello world!');
+});
+
+
+// Problem 2 (Houdini) ------------------------------------------------------
+$('#disappear').click(function() {
+  //WRITE CODE HERE
+});
+
+$('#reappear').click(function() {
+  //WRITE CODE HERE
+});
+
+
+// Problem 3 (Tickle Me Pink) -----------------------------------------------
+// WRITE CODE HERE
+
+
+// Problem 4 (Greet Me) -----------------------------------------------------
+// WRITE CODE HERE
